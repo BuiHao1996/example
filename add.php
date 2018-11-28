de58e775f5bf3e5953ecde7df28536fa9b5364ec
Bui Hao <buithehao1512@gmail.com>
2018-11-28 09:42:07 +0700

    Frist commit

---
+++ add.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="crud.php" value = "add">
+	<div>
+		<label>Username</label>
+		<input type="text" name="username">
+	</div>
+
+	<div>
+		<label>Fullname</label>
+		<input type="text" name="fullname">
+	</div>
+
+	<div>
+		<label>Password</label>
+		<input type="password" name="pwd">
+	</div>
+
+	<div>
+		<label>Email</label>
+		<input type="text" name="email">
+	</div>
+
+	<div>
+		<label>Phone</label>
+		<input type="text" name="phone">
+	</div>
+
+	<div>
+		<label>Address</label>
+		<input type="text" name="address">
+	</div>
+
+
+<input type="submit" name="submit">
+
+
+</form>
+</body>
+</html>
